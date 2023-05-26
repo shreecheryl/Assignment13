9b825c581feda516bb6fe9eea9db20718c146d5f
shreecheryl <shreecheryl@att.net>
2023-05-26 13:23:28 -0700

    bank account completed

--- js/bank-account.js
+++ js/bank-account.js
@@ -3,52 +3,54 @@ const $ = (id) => document.getElementById(id)
 let fullName = $("name")
 let deposit = $("deposit")
 let withdrawal = $("withdrawal")
+let customerInfo = $("withdrawal")
 let customerName = $("customerName")
 let customerBalance = $("customerBalance")
 
-function bankAccount(owner) {
+
+const bankAccount = function (owner) {
     let balance = 0
     let ownerName = owner
     return {
         getOwnerName: function () {
             customerName.innerHTML = ownerName
+            return this
         },
-        getBalance: function (amt) {
-            bankAccount.getOwnerName()
-            customerBalance.innerHTML = `$${amt.toFixed(2)}`
+        getBalance: function () {
+            customerBalance.innerHTML = `$${balance.toFixed(2)}`
+            return this
         },
         makeWithdrawal: function () {
             let withdrawalAmt = parseFloat(prompt("How much are you withdrawing?"))
             if (withdrawalAmt != NaN && withdrawalAmt <= balance ) {
                 balance -= withdrawalAmt
-                bankAccount.getBalance(balance)
+                customerBalance.innerHTML = `$${balance.toFixed(2)}`
             } else {
                 alert("That is not an valid withdrawal amount")
             }
+            return this
         },
         makeDeposit: function () {
             let depositAmt = parseFloat(prompt("How much are you depositing?"))
             if (depositAmt != NaN && depositAmt > 0 ) {
                 balance += depositAmt
-                bankAccount.getBalance(balance)
+                customerBalance.innerHTML = `$${balance.toFixed(2)}`
             }  else {
                 alert("That is not an valid deposit amount")
             }
+            return this
         }
     }  
 }
 
 function getName() {
     let owner = prompt("What is your full name?")
-    try {
-        if (owner != '') {
-            bankAccount = bankAccount(owner)
-            bankAccount.getOwnerName()
-            deposit.addEventListener("click", bankAccount.makeDeposit)
-            withdrawal.addEventListener("click", bankAccount.makeWithdrawal)
-        }
-    } catch(err) {
-        alert("If you are not the owner of the current account, please log out and then log in")
+    if (owner != '') {
+        let newBankAccount = bankAccount(owner)
+        newBankAccount.getBalance()
+        newBankAccount.getOwnerName()
+        deposit.addEventListener("click", newBankAccount.makeDeposit)
+        withdrawal.addEventListener("click", newBankAccount.makeWithdrawal)
     }
 }
 
